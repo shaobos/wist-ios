22ec09468ab46048d05ec6cc574a864a79f8b059
Shaobo Sun <ssb@shaobos-macbook-pro.local>
2016-01-17 15:27:18 -0800

    Initial Commit

---
+++ wist/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  wist
+//
+//  Created by Shaobo Sun on 1/17/16.
+//  Copyright © 2016 Shaobo Sun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
